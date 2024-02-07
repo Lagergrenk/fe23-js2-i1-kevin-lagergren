5e43d7dec5779b31d5fd158c2ac4b5c1e2534a87
Kevin <k.lagergren@live.se>
2024-02-07 21:02:30 +0100

    feat: GameEngine added

--- src/modules/gameEngine.js
+++ src/modules/gameEngine.js
@@ -1,7 +1,48 @@
-function addPlayer(player) {}
+class GameEngine {
+  constructor(player, enemy) {
+    this.player = player;
+    this.enemy = enemy;
+    this.isPlayerTurn = true;
+    this.isGameOver = false;
+  }
 
-function addEnemy(enemy) {}
+  startGame() {
+    this.gameLoop();
+  }
 
-function startGame() {}
+  gameLoop() {
+    while (!this.isGameOver) {
+      if (this.isPlayerTurn) {
+        this.playerTurn();
+      } else {
+        this.enemyTurn();
+      }
+      this.checkGameOver();
+      this.isPlayerTurn = !this.isPlayerTurn;
+    }
+  }
 
-function endGame() {}
+  // TODO : Implement the playerTurn (ATTACK, REST, EQUIP WEAPON)
+  playerTurn() {
+    const damage = this.player.attack(this.player.damage);
+    this.enemy.takeDamage(damage);
+  }
+
+  // Simple enemy turn, just attacks the player
+  enemyTurn() {
+    const damage = this.enemy.attack(this.enemy.damage);
+    if (damage > 0) {
+      this.player.takeDamage(damage);
+    } else {
+      //TODO: Implement the enemy missed message
+    }
+  }
+
+  checkGameOver() {
+    if (!this.player.isAlive()) {
+      this.isGameOver = true;
+    } else if (!this.enemy.isAlive()) {
+      this.isGameOver = true;
+    }
+  }
+}
