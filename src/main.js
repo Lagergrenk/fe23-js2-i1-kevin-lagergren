17ef54cd0511461a10170a03847a9af822e13c4c
Kevin <k.lagergren@live.se>
2024-02-07 19:18:30 +0100

    test: Testing classes and functions

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { Elf } from "./classes/players/races/Elf.js";
 
+import { ArcaneConstruct } from "./classes/enemies/ArcaneConstruct.js";
+
+const construct = new ArcaneConstruct();
+
 const elf = new Elf();
+
+const attack = elf.attack(elf.damage);
+console.log(attack);
+
+console.log(construct);
+
+construct.takeDamage(50);
+console.log(construct);
 
 console.log(elf);
