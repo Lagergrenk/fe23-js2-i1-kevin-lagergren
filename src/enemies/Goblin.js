f7c300c9215e0f7d2a983e4c91330490e9de9fbb
Kevin <k.lagergren@live.se>
2024-02-07 18:20:05 +0100

    feat: New enemies

---
+++ src/enemies/Goblin.js
@@ -0,0 +1,26 @@
+import { Enemy } from "./Enemy.js";
+
+import { checkChance } from "../utils/utils.js";
+
+/*
+ * Goblin Raider
+ * Health: 50
+ * Damage: 5
+ * Hit Chance: 80%
+ *
+ * Abilities: Quick strike
+ * Weakness: Low health
+ *
+ * Chance to hit twice: 20%
+ */
+
+class Goblin extends Enemy {
+  constructor() {
+    super("Goblin Raider", 50, 5, 0.8);
+  }
+
+  isHitTwice() {
+    const hitTwice = checkChance(0.2);
+    return hitTwice;
+  }
+}
