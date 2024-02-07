d8634695de79a32ab9315ba6d9c68698e1a108ce
Kevin <k.lagergren@live.se>
2024-02-07 17:47:56 +0100

    feat: Attack, takeDamage and isAlive
    attack function checkshit chance and returns damage if true

--- src/enemies/Enemy.js
+++ src/enemies/Enemy.js
@@ -1,10 +1,28 @@
 import { Entity } from "../classes/Entity.js";
 
+import { checkChance } from "../utils/utils.js";
+
 class Enemy extends Entity {
   constructor(name, health, damage, hitChance) {
     super(name, health);
     this.damage = damage;
     this.hitChance = hitChance;
+  }
+
+  attack(target) {
+    if (checkChance(this.hitChance)) {
+      return this.damage;
+    } else {
+      return 0;
+    }
+  }
+
+  takeDamage(damage) {
+    this.health -= damage;
+  }
+
+  isAlive() {
+    return this.health > 0;
   }
 }
 
