670981e66c63fd4cac71ceadc85eb3c52822926a
Kevin <k.lagergren@live.se>
2024-02-08 15:45:49 +0100

    refactor: Comments

--- src/classes/players/races/Dwarf.js
+++ src/classes/players/races/Dwarf.js
@@ -1,5 +1,13 @@
 import { Player } from "../Player.js";
 
+/**
+ * Class representing a Dwarf.
+ * Dwarf
+ * maxHealth: 100
+ * attack: 40
+ * hitChance: 0.8
+ */
+
 class Dwarf extends Player {
   constructor() {
     super("Dwarf", 100, 40, 0.8);
