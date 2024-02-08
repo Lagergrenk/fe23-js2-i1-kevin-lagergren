2e53fbb78623b9a7c227130dae95a2db28e10b8b
Kevin <k.lagergren@live.se>
2024-02-08 15:46:18 +0100

    feat: Racebutton working, changing stats and image

---
+++ src/modules/eventlistners.js
@@ -0,0 +1,51 @@
+import { Dwarf } from "../classes/players/races/Dwarf.js";
+import { Elf } from "../classes/players/races/Elf.js";
+import { Human } from "../classes/players/races/Human.js";
+import { Gnome } from "../classes/players/races/Gnome.js";
+
+const raceButton = document.querySelectorAll(".race-container");
+const playerImage = document.querySelector("#player");
+
+raceButton.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const player = playerChoice(e);
+    const enemy = new Dwarf("Dwarf", 100, 10, 10, 10);
+  });
+});
+
+function playerChoice(e) {
+  let player;
+  switch (e.target.id) {
+    case "dwarf":
+      player = new Dwarf();
+      playerImage.src = "../assets/images/dwarf-front-facing.webp";
+      changeStats(player);
+      break;
+    case "elf":
+      player = new Elf();
+      playerImage.src = "../assets/images/elf-front-facing.webp";
+      changeStats(player);
+      break;
+    case "human":
+      player = new Human();
+      playerImage.src = "../assets/images/human-front-facing.webp";
+      changeStats(player);
+      break;
+    case "gnome":
+      player = new Gnome();
+      playerImage.src = "../assets/images/gnome-front-facing.webp";
+      changeStats(player);
+      break;
+    default:
+      break;
+  }
+  return player;
+}
+
+function changeStats(player) {
+  const playerStats = document.querySelectorAll(".user-stats");
+  playerStats[0].textContent = `Level: ${player.level}`;
+  playerStats[1].textContent = `Health: ${player.maxHealth}`;
+  playerStats[2].textContent = `Damage: ${player.damage}`;
+  playerStats[3].textContent = `Hitchance: ${player.hitChance * 100}%`;
+}
