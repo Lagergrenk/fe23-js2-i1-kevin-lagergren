e2baebb93d48a05d2126af5881a966d84fdeeaf3
Kevin <k.lagergren@live.se>
2024-02-13 12:36:25 +0100

    chore: Removed display module

--- src/modules/display.js
+++
@@ -1,9 +0,0 @@
-function show(module) {
-  document.querySelector(`.${module}`).style.display = "block";
-}
-
-function hide(module) {
-  document.querySelector(`.${module}`).style.display = "none";
-}
-
-export { show, hide };
