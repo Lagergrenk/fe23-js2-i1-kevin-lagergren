b25672ccd3ecb8b469c41c6c8740567b9d374bec
Kevin <k.lagergren@live.se>
2024-02-13 12:38:33 +0100

    refactor/chore: Moved changestats to utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,4 +3,12 @@ function checkChance(chance) {
   return checkChance;
 }
 
-export { checkChance };
+function changeStats(playerObject) {
+  const playerStats = document.querySelectorAll(".user-stats");
+  playerStats[0].textContent = `Level: ${playerObject.level}`;
+  playerStats[1].textContent = `Health: ${playerObject.maxHealth}`;
+  playerStats[2].textContent = `Damage: ${playerObject.damage}`;
+  playerStats[3].textContent = `Hitchance: ${playerObject.hitChance * 100}%`;
+}
+
+export { checkChance, changeStats };
