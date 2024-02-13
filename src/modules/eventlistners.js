34ae65aa8adfcffcd1d9224d0c3960f189721d32
Kevin <k.lagergren@live.se>
2024-02-13 12:37:12 +0100

    feat/refactor: Improved logic and refactoring

--- src/modules/eventlistners.js
+++ src/modules/eventlistners.js
@@ -1,75 +1,61 @@
-import { GameEngine } from "./gameEngine.js";
+import { uiManager } from "./UIManager.js";
+import { gameEngine } from "./gameEngine.js";
 
-import { Dwarf } from "../classes/players/races/Dwarf.js";
-import { Elf } from "../classes/players/races/Elf.js";
-import { Human } from "../classes/players/races/Human.js";
-import { Gnome } from "../classes/players/races/Gnome.js";
-
-const raceButton = document.querySelectorAll(".race-container");
-const playerImage = document.querySelector("#player");
-const form = document.querySelector(".player-name form");
+let gameMode = "";
+let selectedRace = "";
+let playerName = "";
 
 function initEventListeners() {
-  const defaultChoice = new Human();
-  playerImage.src = defaultChoice.image;
-  playerImage.alt = "Human";
-  changeStats(defaultChoice);
-  raceButton.forEach((button) => {
+  uiManager.showInitialChoice();
+  const gameModeChoices = document.querySelectorAll(
+    ".one-player-or-two-player"
+  );
+
+  gameModeChoices.forEach((button) => {
     button.addEventListener("click", (e) => {
-      const player = playerChoice(e);
+      gameMode = e.target.id;
+
+      if (gameMode == "pvp") {
+        console.log("not implemented yet");
+      } else {
+        uiManager.showRaceAndNameInput();
+        // Default preview
+        uiManager.showPlayerRacePreview("human");
+      }
     });
   });
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const playerName = document.querySelector("#player-name").value;
-    const playerRace = document.querySelector("#player").alt.toLowerCase();
-    console.log("playerName = " + playerName);
-    console.log("playerRace = " + playerRace);
-
-    window.location.href = "/pages/game.html";
+  const raceButtons = document.querySelectorAll(".race-button");
+  raceButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      selectedRace = e.target.id;
+      uiManager.showPlayerRacePreview(selectedRace);
+    });
   });
-}
 
-function playerChoice(e) {
-  let player;
-  switch (e.target.id) {
-    case "dwarf":
-      player = new Dwarf();
-      playerImage.src = player.image;
-      playerImage.alt = "Dwarf";
-      changeStats(player);
-      break;
-    case "elf":
-      player = new Elf();
-      playerImage.src = player.image;
-      playerImage.alt = "Elf";
-      changeStats(player);
-      break;
-    case "human":
-      player = new Human();
-      playerImage.src = player.image;
-      playerImage.alt = "Human";
-      changeStats(player);
-      break;
-    case "gnome":
-      player = new Gnome();
-      playerImage.src = player.image;
-      playerImage.alt = "Gnome";
-      changeStats(player);
-      break;
-    default:
-      break;
-  }
-  return player;
-}
+  const playerForm = document.querySelector(".player-name form");
+  playerForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    playerName = document.querySelector("#player-name").value;
+    document.querySelector("#player-name h1").textContent = playerName;
+    gameEngine.createPlayer(selectedRace);
+    gameEngine.createEnemy();
+    uiManager.showGameView();
+  });
 
-function changeStats(player) {
-  const playerStats = document.querySelectorAll(".user-stats");
-  playerStats[0].textContent = `Level: ${player.level}`;
-  playerStats[1].textContent = `Health: ${player.maxHealth}`;
-  playerStats[2].textContent = `Damage: ${player.damage}`;
-  playerStats[3].textContent = `Hitchance: ${player.hitChance * 100}%`;
+  const optionsButton = document.querySelectorAll(".options-container");
+  optionsButton.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      let playerAction = e.target.id;
+      gameEngine.performPlayerAction(playerAction);
+    });
+  });
+
+  const playAgainButton = document.querySelector("#play-again");
+  playAgainButton.addEventListener("click", (e) => {
+    gameEngine.resetGame();
+    uiManager.showInitialChoice();
+  });
 }
 
 export { initEventListeners };
