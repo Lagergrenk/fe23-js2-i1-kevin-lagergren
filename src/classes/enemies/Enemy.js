a2cdfcf961fc587217961498fcd81f2ee895700b
Kevin <k.lagergren@live.se>
2024-02-13 12:39:34 +0100

    feat: Attack raturns dmg and message

--- src/classes/enemies/Enemy.js
+++ src/classes/enemies/Enemy.js
@@ -1,8 +1,25 @@
 import { Entity } from "../entities/Entity.js";
+import { checkChance } from "../../utils/utils.js";
 
 class Enemy extends Entity {
   constructor(name, maxHealth, damage, hitChance, image) {
     super(name, maxHealth, damage, hitChance, image);
+  }
+
+  attack() {
+    const hit = checkChance(this.hitChance);
+
+    if (hit) {
+      return {
+        damage: this.damage,
+        message: `${this.name} attacks for ${this.damage} damage!`,
+      };
+    } else {
+      return {
+        damage: 0,
+        message: `${this.name} missed!`,
+      };
+    }
   }
 }
 
