b2940c79be766570db38c312d2af82db4292f37f
Kevin <k.lagergren@live.se>
2024-02-13 12:09:57 +0100

    chore: Easier imports

---
+++ src/classes/enemies/index.js
@@ -0,0 +1,6 @@
+export { Goblin } from "./Goblin.js";
+export { ForestTroll } from "./ForestTroll.js";
+export { UndeadWarrior } from "./UndeadWarrior.js";
+export { ElementalMage } from "./ElementalMage.js";
+export { ArcaneConstruct } from "./ArcaneConstruct.js";
+export { ShadowAssasin } from "./ShadowAssasin.js";
---
+++ src/classes/players/races/index.js
@@ -0,0 +1,4 @@
+export { Dwarf } from "./Dwarf.js";
+export { Elf } from "./Elf.js";
+export { Gnome } from "./Gnome.js";
+export { Human } from "./Human.js";
