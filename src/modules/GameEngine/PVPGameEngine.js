bc660f92b6bd81fe571edc8926a2cb600521906c
Kevin <k.lagergren@live.se>
2024-02-14 15:39:18 +0100

    feat: Player v Player

---
+++ src/modules/GameEngine/PVPGameEngine.js
@@ -0,0 +1,126 @@
+import { Human, Elf, Gnome, Dwarf } from "../../classes/players/races/index.js";
+import { GameEngine } from "./GameEngine.js";
+import { uiManager } from "../UIManager.js";
+
+class PVPGameEngine extends GameEngine {
+  constructor() {
+    super();
+    this.player1 = null;
+    this.player2 = null;
+    this.isPlayer1sTurn = true;
+    this.player1Name = "";
+    this.player2Name = "";
+  }
+
+  initializePlayers(player1Race, player2Race, player1Name, player2Name) {
+    this.player1 = this.createPlayer(player1Race, player1Name);
+    this.player2 = this.createPlayer(player2Race, player2Name);
+
+    this.player1Name =
+      player1Name.charAt(0).toUpperCase() + player1Name.slice(1);
+    this.player2Name =
+      player2Name.charAt(0).toUpperCase() + player2Name.slice(1);
+
+    uiManager.updatePlayerInfo(this.player1);
+    uiManager.updateEnemyInfo(this.player2);
+
+    this.startGame();
+  }
+
+  createPlayer(selectedRace, name) {
+    const raceClassMap = { human: Human, elf: Elf, gnome: Gnome, dwarf: Dwarf };
+    const raceClass = raceClassMap[selectedRace];
+    if (raceClass) {
+      return new raceClass(name);
+    } else {
+      console.error("Invalid race selected:", selectedRace);
+    }
+  }
+
+  startGame() {
+    uiManager.showGameView();
+    uiManager.updateChatBoxWithMessage(`${this.player1Name}'s turn...`);
+    uiManager.updatePlayerInfo(this.player1, this.player1Name);
+    uiManager.updatePlayerForBattle(this.player1);
+    uiManager.updateEnemyInfo(this.player2, this.player2Name);
+    uiManager.updateEnemyForBattle(this.player2);
+  }
+
+  performPlayerAction(action) {
+    const player = this.isPlayer1sTurn ? this.player1 : this.player2;
+    if (this.isGameover) return;
+
+    console.log(this.player1.isAlive(), this.player2.isAlive());
+
+    switch (action) {
+      case "attack":
+        this.performAttack(player);
+        break;
+      case "rest":
+        this.performRest(player);
+        break;
+      case "special-attack":
+        this.performSpecialAttack(player);
+        break;
+      default:
+        break;
+    }
+    this.checkGameOver();
+
+    if (!this.isGameover) {
+      this.isPlayer1sTurn = !this.isPlayer1sTurn;
+
+      const nextPlayerName = this.isPlayer1sTurn
+        ? this.player1Name
+        : this.player2Name;
+      uiManager.updateChatBoxWithMessage(`${nextPlayerName}'s turn...`);
+    }
+  }
+
+  performAttack(player) {
+    const opponent = player === this.player1 ? this.player2 : this.player1;
+    const result = player.attack();
+    opponent.takeDamage(result.damage);
+    if (opponent === this.player1) {
+      uiManager.updatePlayerInfo(opponent, this.player1Name);
+    } else {
+      uiManager.updateEnemyInfo(opponent, this.player2Name);
+    }
+    uiManager.updateChatBoxWithMessage(result.message);
+  }
+
+  performRest(player) {
+    const result = player.rest();
+    uiManager.updateChatBoxWithMessage(result.message);
+    uiManager.updatePlayerInfo(this.player1, this.player1Name);
+    uiManager.updateEnemyInfo(this.player2, this.player2Name);
+  }
+
+  performSpecialAttack(player) {
+    const opponent = player === this.player1 ? this.player2 : this.player1;
+    const result = player.specialAttack();
+    opponent.takeDamage(result.damage);
+    uiManager.updateEnemyInfo(opponent);
+    uiManager.updateChatBoxWithMessage(result.message);
+  }
+
+  checkGameOver() {
+    if (!this.player1.isAlive() || !this.player2.isAlive()) {
+      this.endGame();
+    } else {
+      console.log("Game is still going!");
+    }
+  }
+
+  endGame() {
+    this.isGameover = true;
+    const message = !this.player1.isAlive()
+      ? `${this.player1Name} has been defeated! ${this.player2Name} wins!`
+      : `${this.player2Name} has been defeated! ${this.player1Name} wins!`;
+    uiManager.updateChatBoxWithMessage(message);
+    uiManager.showPlayAgainButton();
+  }
+}
+
+const pvpGameEngine = new PVPGameEngine();
+export { pvpGameEngine };
--- src/modules/UIManager.js
+++ src/modules/UIManager.js
@@ -11,9 +11,12 @@ class UIManager {
     this.gameWrapper = document.querySelector(".game-wrapper");
     this.playerImage = document.querySelector("#player");
     this.chatBox = document.querySelector(".chat-message");
+    this.optionsContainer = document.querySelector(".options-container");
+    this.playAgainContainer = document.querySelector(".play-again-container");
 
     this.isTyping = false;
     this.typingTimeoutId = null;
+    this.messageQueue = [];
   }
 
   showInitialChoice() {
@@ -27,7 +30,7 @@ class UIManager {
       this.gameWrapper
     );
   }
-  showRaceAndNameInput() {
+  showRaceAndNameInput(gameMode) {
     this.playerSelection.style.display = "grid";
     this.show(
       this.raceContainer,
@@ -38,14 +41,15 @@ class UIManager {
     this.hide(this.playerVsContainer, this.gameWrapper);
   }
   showGameView() {
-    this.show(this.gameWrapper);
+    this.show(this.gameWrapper, this.optionsContainer);
     this.hide(
       this.playerSelection,
       this.playerVsContainer,
       this.raceContainer,
       this.playerImageDiv,
       this.playerNameSection,
-      this.statsContainer
+      this.statsContainer,
+      this.playAgainContainer
     );
   }
   showPlayerRacePreview(selectedRace) {
@@ -74,7 +78,8 @@ class UIManager {
     }
   }
 
-  updatePlayerInfo(playerObj) {
+  updatePlayerInfo(playerObj, playerName) {
+    document.querySelector("#player-name h1").textContent = playerName;
     document.querySelector(
       "#player-health"
     ).textContent = `${playerObj.currentHealth}/${playerObj.maxHealth}`;
@@ -87,8 +92,8 @@ class UIManager {
     playerSprite.src = `${player.backImagePath}`;
   }
 
-  updateEnemyInfo(enemy) {
-    document.querySelector("#enemy-name h1").textContent = enemy.name;
+  updateEnemyInfo(enemy, enemyName = enemy.name) {
+    document.querySelector("#enemy-name h1").textContent = enemyName;
     document.querySelector(
       "#enemy-health"
     ).textContent = `${enemy.currentHealth}/${enemy.maxHealth}`;
@@ -102,29 +107,52 @@ class UIManager {
     enemySprite.src = enemy.image;
   }
 
-  updateChatBoxWithMessage(message) {
-    const chatBox = document.querySelector(".chat-message");
-    chatBox.textContent = "";
-
-    if (this.isTyping && this.typingTimeoutId) {
-      clearTimeout(this.typingTimeoutId);
-      chatBox.textContent = "";
+  proccessMessageQueue() {
+    if (this.messageQueue.length === 0) {
+      this.isTyping = false;
+      this.toggleOptionsContainer(true);
+      return;
     }
 
     this.isTyping = true;
+    this.toggleOptionsContainer(false);
+    const message = this.messageQueue.shift();
+    this.chatBox.textContent = "";
     let index = 0;
 
     const typeLetterByLetter = () => {
       if (index < message.length) {
-        chatBox.textContent += message.charAt(index);
+        this.chatBox.textContent += message.charAt(index);
         index++;
-        this.typingTimeoutId = setTimeout(typeLetterByLetter, 50);
+        setTimeout(typeLetterByLetter, 50);
       } else {
-        this.isTyping = false;
+        setTimeout(() => {
+          if (this.messageQueue.length > 0) {
+            this.proccessMessageQueue();
+          } else {
+            this.isTyping = false;
+            this.toggleOptionsContainer(true);
+          }
+        }, 1000);
       }
     };
 
     typeLetterByLetter();
+  }
+
+  updateChatBoxWithMessage(message) {
+    this.messageQueue.push(message);
+    if (!this.isTyping) {
+      this.proccessMessageQueue();
+    }
+  }
+
+  toggleOptionsContainer(show) {
+    if (this.playAgainContainer.style.display === "flex") {
+      this.optionsContainer.style.display = "none";
+    } else {
+      this.optionsContainer.style.display = show ? "flex" : "none";
+    }
   }
 
   changeStats(player) {
@@ -136,10 +164,16 @@ class UIManager {
   }
 
   showPlayAgainButton() {
-    const playAgainContainer = document.querySelector(".play-again-container");
-    const optionsContainer = document.querySelector(".options-container");
-    this.show(playAgainContainer);
-    this.hide(optionsContainer);
+    this.toggleOptionsContainer(false);
+    this.show(this.playAgainContainer);
+  }
+
+  changePlayerLabelForPVP(playerNumber) {
+    const formSection = document.querySelector(".player-name");
+    const playerNameLabel = formSection.querySelector(
+      "label[for='player-name']"
+    );
+    playerNameLabel.textContent = `Player ${playerNumber} Name:`;
   }
   show(...elements) {
     elements.forEach((element) => (element.style.display = "flex"));
@@ -147,6 +181,11 @@ class UIManager {
 
   hide(...elements) {
     elements.forEach((element) => (element.style.display = "none"));
+  }
+
+  showPlayerSelectionPVP(playerNumber) {
+    this.showRaceAndNameInput();
+    this.changePlayerLabelForPVP(playerNumber);
   }
 }
 
